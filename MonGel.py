01b8e1b59670228248cb24047b791457a759d7d5
fleury <guilherme.fleury26@gmail.com>
2024-03-26 15:13:08 -0300

    automatic conection WIP
    no exeption treatment for second bluetooth serial port

--- MonGel.py
+++ MonGel.py
@@ -1,20 +1,20 @@
 import serial.tools.list_ports
 import MonGel_Functions
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 import sys
 
 
-ports = MonGel_Functions.get_ports()
-MonGel_port = MonGel_Functions.findMonGel()
-MonGel_port = ports[1]
+commPort = MonGel_Functions.connectBluetooth()
+print(commPort)
+ser = serial.Serial(commPort,baudrate = 115200,timeout=1,exclusive=True)
 
 
 
-ser = serial.Serial('COM5',baudrate = 115200,timeout=1,exclusive=True)
 
 
 while True:
     value= ser.readline()
     valueInString=str(value,'UTF-8')
     print(valueInString)
+    ser.write("OLA\r\n".encode())
--- MonGel_Functions.py
+++ MonGel_Functions.py
@@ -1,10 +1,34 @@
 import serial.tools.list_ports
+from time import sleep
 
 def get_ports ():
     ports = serial.tools.list_ports.comports()
     return ports
 
-def findMonGel ():
+def connectUART ():
+    ports = serial.tools.list_ports.comports() #array with port list
+    commPort = 'None'
+    numConnection = len(ports)
+    for i in range(0,numConnection):
+        port = ports[i]
+        strPort = str(port)
+
+        if "MSP Application UART1" in strPort:
+            splitPort = strPort.split(' ')
+            commPort = (splitPort[0])
+            ser = serial.Serial(commPort,baudrate = 115200,timeout=1,exclusive=True)
+            ser.write("!\r\n".encode())
+            sleep(0.005)
+            response = ser.readline()
+            if response == "\r\nUART\r\n":
+                return commPort
+
+
+    
+    return commPort
+
+
+def connectBluetooth ():
     ports = serial.tools.list_ports.comports() #array with port list
     commPort = 'None'
     numConnection = len(ports)
@@ -15,6 +39,15 @@ def findMonGel ():
         if 'Bluetooth' in strPort:
             splitPort = strPort.split(' ')
             commPort = (splitPort[0])
-            print(splitPort)
+            ser = serial.Serial(commPort,baudrate = 115200,timeout=1,exclusive=True)
+            ser.write("!\r\n".encode())
+            while True:
+                response = ser.readline()
+                responseString = str(response,'UTF-8')
+                print(responseString)
+                if responseString == "BLUE\r\n":
+                    return commPort
+
+
     
     return commPort
